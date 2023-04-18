54193dcd22ca1db21ca111d7d379c68fccfb0e4a
Pankratov Semen <68625468+7enSmile@users.noreply.github.com>
2023-04-18 12:30:22 +0000

    refactor

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='torquepy',
-    version='0.2',
+    version='0.3.1.1',
     description='Torque library',
     author='Semen Pankratov',
     author_email='sema2012r@mail.ru',
